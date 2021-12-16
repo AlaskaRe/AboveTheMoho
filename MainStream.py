4e46f099d4d341e5c4b70ef97afd3933e8bb5230
stuart alaska <wukong14@outlook.com>
2021-12-16 19:13:07 +0800

    换座驾

--- MainStream.py
+++ MainStream.py
@@ -1,43 +1,53 @@
 from openpyxl import load_workbook
+from openpyxl.descriptors.base import Length
 
-Point = {}
-stratums_lib = []
 
 wb = load_workbook(filename="./RawData/RawPointData.xlsx", data_only=True)
 wksht_pt = wb["POSTION"]
 wksht_lib = wb["LAYER_LIBARY"]
 wksht_args = wb["POINT_ARGS"]
 
+# 1.Read the data from the excel, and generate the Point and Stratums_lib;
+Point = {}
+Stratums_lib = []
+
+# 1.1 Generate the Stratums_lib;
 # read the worksheet"LAYER_LIBARY", get the information of the layers
 for row in wksht_lib.iter_rows(min_row=2, max_row=20, max_col=3):
+
     if row[0].value != None:
 
-        T = (row[0].value, row[1].value, row[2].value)
-        stratums_lib.append(T)
-# If I define the layerInformation outside the for loop, then every point get the adress of this list,
-# then everytime I give the value to the point , it will modify the layerinformation, every point has the same list value
-# layerInformation = [None for i in range(len(stratums_lib))]
+        T = (row[0].value, row[1].value)
+        Stratums_lib.append(T)
 
+# 1.2 Generate the Point;
 # Read the worksheet "POSTION",get the point data.
 for r in wksht_pt.iter_rows(min_row=2, max_col=3):
+
     if r[0].value == None:
+
         break
-    layerInformation = [None for i in range(len(stratums_lib))]
-    Point[r[0].value, r[1].value, r[2].value] = layerInformation
+
+    layerInformation = [None for i in range(len(Stratums_lib))]
+    Point[r[0].value, r[1].value] = layerInformation
 
 # Read the worksheet "POINT_ARGS", get the stratums of each point.
 for r in wksht_args.iter_rows(min_row=2, max_col=13):
 
     if r[0].value == None:
+
         break
+
     else:
         # Read the row and get the value of every stratum for each point.
-        kywd = (r[2].value, r[3].value, r[12].value)
-        da = [r[7].value, r[8].value, r[9].value]
-        idx = None
-        for i in stratums_lib:
+
+        kywd = (r[2].value, r[3].value)
+        da = [r[7].value, r[8].value, r[11].value]
+        Point[kywd]
+        layers = len(Stratums_lib)
+        for i in Stratums_lib:
             if i[0] == r[11].value:
-                idx = stratums_lib.index(i)
+                idx = Stratums_lib.index(i)
                 break
 
         # give the stratum value to the dict if not exists, else add another value to this list
@@ -52,4 +62,4 @@ for r in wksht_args.iter_rows(min_row=2, max_col=13):
             t.append(da)
             Point[kywd][idx] = t
 
-print(Point)
+# print(Point)
